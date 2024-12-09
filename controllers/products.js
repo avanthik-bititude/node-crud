4a7619c07eed355e44634fbc26821de5551fd93c
avanthik <avanthik33@gmail.com>
2024-12-09 15:04:28 +0530

    added code for products testing

--- controllers/products.js
+++ controllers/products.js
@@ -4,15 +4,16 @@ import { matchedData } from "express-validator";
 export const viewAllProducts = async (req, res) => {
   try {
     const products = await ProductsModel.findAll();
-    if (Array.isArray(products) && products.length > 0) {
-      return res.status(200).json({
-        status: "success",
-        data: products,
+    console.log(products);
+    if (products.length === 0) {
+      return res.status(404).json({
+        status: "error",
+        message: "No products found",
       });
     }
-    return res.status(204).json({
+    return res.status(200).json({
       status: "success",
-      message: "no data found",
+      data: products,
     });
   } catch (error) {
     return res.status(500).json({
@@ -26,11 +27,6 @@ export const viewAllProducts = async (req, res) => {
 export const addProducts = async (req, res) => {
   try {
     const { name, description } = matchedData(req);
-    if ((!name, !description)) {
-      return res.status(404).json({
-        status: "error",
-      });
-    }
     await ProductsModel.create({
       name,
       description,
